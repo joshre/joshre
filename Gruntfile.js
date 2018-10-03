d5b29f9ffe9c1607e4d9ab48d1bd03c3f3f05d2d
joshre <joshre@me.com>
2018-10-02 17:00:18 -0700

    Months

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+const sass = require('node-sass');
+var Fiber = require("fibers");
 module.exports = function(grunt) {
     grunt.initConfig({
         conf: {
@@ -22,8 +24,9 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 options: {
-                    style: 'nested',
-                    sourcemap: 'none'
+                    implementation: sass,
+                    fiber: Fiber,
+                    sourceMap: false
                 },
                 files: {
                     '<%= conf.app %>/main.min.css': 'scss/main.scss',
@@ -48,20 +51,18 @@ module.exports = function(grunt) {
                 }
             }
         },
-        postcss: {
-            options: {
-                processors: [
-                    require('postcss-font-magician')({
-                        hosted: ['app/fonts']
-                    })
-                ]
-            },
-            dist: {
-                files: {
-                    '<%= conf.app %>/main.min.css': '<%= conf.app %>/main.min.css'
-                }
-            }
-        },
+        // postcss: {
+        //     options: {
+        //         processors: [
+        //             require('postcss-font-magician')({
+        //                 hosted: '../fonts/'
+        //             })
+        //         ]
+        //     },
+        //     dist: {
+        //         src: '<%= conf.app %>/main.min.css'
+        //     }
+        // },
         watch: {
             twig: {
                 files: '**/*.twig',
@@ -79,6 +80,18 @@ module.exports = function(grunt) {
                 options: {
                     livereload: true,
                 },
+            },
+            cssnano: {
+                files: ["<%= conf.sass %>"],
+                tasks: ["cssnano"]
+            },
+            svgmin: {
+                files: ["<%= conf.iconts %>/*.svg"],
+                tasks: ["svgmin:dist"]
+            },
+            grunticon: {
+                files: ["<%= conf.icons %>/optimized/*.svg"],
+                tasks: ["grunticon:myIcons", "copy"]
             }
         },
         svgmin: {
@@ -156,6 +169,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.registerTask('default', ['watch', 'notify_hooks']);
     grunt.registerTask('icons', ['svgmin', 'grunticon', 'clean', 'copy']);
-    grunt.registerTask('server', ['uglify', 'sass', 'cssnano', 'postcss', 'svgmin', 'grunticon', 'clean', 'copy']);
+    grunt.registerTask('server', ['uglify', 'sass', 'cssnano', 'svgmin', 'grunticon', 'clean', 'copy']);
     grunt.task.run('notify_hooks');
 }
